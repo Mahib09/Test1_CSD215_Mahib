44ef1925c133156622430a6862c3010de91ed5bc
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-03-22 13:40:09 -0400

    calculate tax for all salaries based on the table provided

--- Program.fs
+++ Program.fs
@@ -2,4 +2,15 @@
 printfn "Base Salaries without Taxes: %A" salaries
 let highIncomeSalaries = salaries |> List.filter (fun salary -> salary > 100000)
 printfn "High-income salaries: %A" highIncomeSalaries
+let calculateTax salary =
+    match salary with
+    | s when s <= 49020 -> int (float salary * 0.15) 
+    | s when s <= 98040 -> int (float salary * 0.205)
+    | s when s <= 151978 -> int (float salary * 0.26)
+    | s when s <= 216511 -> int (float salary * 0.29)
+    | _ -> int (float salary * 0.33)
+let taxes = salaries |> List.map calculateTax
+printfn "Taxes for all salaries: %A" taxes
+let salariesAfterTax = List.map2 (+) salaries taxes
+printfn "Salaries with taxes: %A" salariesAfterTax
 
